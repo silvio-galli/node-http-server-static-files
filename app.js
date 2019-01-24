318e576f7d2a434915377d51ea56c3014de6e0da
Silvio Galli <silvio.galli@gmail.com>
2019-01-24 14:05:45 +0100

    Added primordial server instance

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("Hello world")
+const http = require('http')
+const PORT = 3000
+const server = http.createServer((req, res) => {
+  res.write("hello world")
+  res.end()
+})
+
+server.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
