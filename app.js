51eb50626d6aa2d8593675a32d68450e7e77c019
Silvio Galli <silvio.galli@gmail.com>
2019-01-24 14:27:43 +0100

    Sending html file

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-const http = require('http')
 const PORT = 3000
+const http = require('http')
+const fs = require('fs')
+const basedir = __dirname + '/public/'
+
 const server = http.createServer((req, res) => {
-  res.write("hello world")
-  res.end()
+  console.log("req.url ->", req.url)
+  fs.readFile(basedir + 'index.html', 'utf8', (err, data) => {
+    if (err) {
+      console.log(err)
+    } else {
+      res.writeHead(200, {
+        'Content-type': 'text/html'
+      })
+      res.write(data)
+      res.end()
+    }
+  })
 })
 
 server.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
